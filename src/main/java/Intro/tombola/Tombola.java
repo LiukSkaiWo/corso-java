596a929e45d85489051e42221d2db98e0e293708
LiukSkaiWo <albergo.vargroup@gmail.com>
2023-07-31 16:48:15 +0200

    Migliorato il codice richiamando i vari metodi

--- src/main/java/Intro/tombola/Tombola.java
+++ src/main/java/Intro/tombola/Tombola.java
@@ -4,17 +4,24 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Tombola {
-    public static void main(String[] args) {
 
-        Random generator = new Random();
-        // Costruzione arraylist con i 90 numeri
-        ArrayList<Integer> numbers = new ArrayList<>();
+    //Creo la arraylist
+    private ArrayList <Integer> numbers = new ArrayList<>();
+
+    //Popolo la arraylist
+    private void initList(){
         for (int i = 1; i < 91; i++) {
             numbers.add(i);
         }
         System.out.println("Totale numeri prima = "+numbers.size());
+    }
 
-        //genero 20 numeri casuali, li stampo e li rimuovo
+    //Creo il metodo per fare l'estrazione richiamando l'altro metodo
+    public void extract(){
+    // richiamo il metodo per inizializzare la lista
+        initList();
+
+        Random generator = new Random();
 
         for (int i = 1; i < 21; i++) {
             int n = 1+ generator.nextInt(numbers.size());
@@ -23,7 +30,12 @@ public class Tombola {
         System.out.println();
         System.out.println("Totale numeri aggiornato = "+numbers.size());
         System.out.print(numbers);
+    }
 
-
+    public static void main(String[] args) {
+        //Creo un oggetto Tombola
+        Tombola t = new Tombola();
+        //Richiamo il metodo public
+        t.extract();
     }
 }
